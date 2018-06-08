abc51fdefc5cbe9f6c8560890b23370dcf88717b
pengjiangyi <pengjiangyi@shanghai3h.com>
2018-06-08 21:39:35 +0800

    Init project

---
+++ LinearAlgebra/Sailias.Math/Sailias.Math/Matrix.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sailias.Math
+{
+    public class Matrix
+    {
+        /// <summary>
+        /// 矩阵构造方法，如a[0][0]=1,a[0][1]=2,a[1][0]=-1,a[1][1]=1 格式为Matrix(2,2,[1,2,-1,1]) 若值不够，则默认填充0
+        /// </summary>
+        /// <param name="RowCount"></param>
+        /// <param name="ColCount"></param>
+        /// <param name="values"></param>
+        public Matrix(int RowCount,int ColCount, double[] values){
+            this._rowCount = RowCount;
+            this._colCount = ColCount;
+            this._value = new double[RowCount][];
+          //  this._value[this.RowCount] =new double[this._colCount];
+            int length = 0;
+            for (int i = 0; i < RowCount; i++)
+            {
+                this._value[i] = new double[ColCount];
+                for (int j = 0; j < ColCount; j++)
+                {
+                    if (values.Length > length)
+                        this._value[i][j] = values[length];
+                    else this._value[i][j] = 0;
+                    length++;
+                }
+            }
+        }
+        private int _rowCount;
+        /// <summary>
+        /// 矩阵的行数
+        /// </summary>
+        public int RowCount
+        {
+            get
+            {
+                return this._rowCount;
+            }
+        }
+
+        private int _colCount;
+        /// <summary>
+        /// 矩阵的列数 
+        /// </summary>
+        public int ColCount
+        {
+            get
+            {
+                return this._colCount;
+            }
+        }
+        private double[][] _value;
+        public double[][] Value
+        {
+            get
+            {
+                return this._value;
+            }
+        }
+        private Matrix _rank;
+        public Matrix Rank
+        {
+            get
+            {
+                return this._rank;
+            }
+        }
+    }
+}
---
+++ LinearAlgebra/Sailias.Math/TestForm/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sailias.Math;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Matrix TextMatix;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Clear();
+            int row = int.Parse(this.rowInput.Text);
+            int col = int.Parse(this.colInput.Text);
+            string valueText = this.valueInput.Text.ToString();
+            string[] values = valueText.Split(',');
+            double[] relValues = new double[values.Length];
+            int i = 0;
+            foreach (var item in values)
+            {
+                relValues[i] = double.Parse(item);
+                i++;
+            }
+            TextMatix = new Matrix(row, col, relValues);
+            for (int j = 0; j < TextMatix.RowCount; j++)
+            {
+                if (j > 0)
+                    this.richTextBox1.Text += "\n";
+                for (int k = 0; k < TextMatix.ColCount; k++)
+                {
+                    if (k != TextMatix.ColCount - 1)
+                        this.richTextBox1.Text += TextMatix.Value[j][k] + ",";
+                    else  this.richTextBox1.Text += TextMatix.Value[j][k];
+                }
+
+            }
+
+
+
+        }
+    }
+}
